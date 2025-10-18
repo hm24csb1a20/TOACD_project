a2b4d08c3f0a28c0d3f3c9d7b6fdae6e57b883f5
hm24csb1a20 <hm24csb1a20@student.nitw.ac.in>
2025-10-18 20:24:23 +0530

    added os to search ocrrect directoroy

--- compiller_main.py
+++ compiller_main.py
@@ -6,6 +6,9 @@ LEXER_DEBUG :bool= True
 
 
 if __name__=='__main__':
+    
+    current = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current,"test.lime")
     with open("test.lime","r") as file :
         code :str = file.read()
         if(LEXER_DEBUG):
