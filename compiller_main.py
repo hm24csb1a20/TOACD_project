d64fbe1b69ae63287b7fc4996768ada8e12f2936
hm24csb1a20 <hm24csb1a20@student.nitw.ac.in>
2025-10-18 20:23:58 +0530

    just testing the added lexical anayliser

---
+++ compiller_main.py
@@ -0,0 +1,17 @@
+from lexer import Lexer
+import os
+
+LEXER_DEBUG :bool= True
+
+
+
+if __name__=='__main__':
+    with open("test.lime","r") as file :
+        code :str = file.read()
+        if(LEXER_DEBUG):
+            debug_lex: Lexer = Lexer(source=code)
+        while(debug_lex.current_char is not None):
+            print(debug_lex.next_token())
+
+    
+    
