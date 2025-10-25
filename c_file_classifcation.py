4f425343ef4f01701552f545ba4ff575c0c5ced1
hm24csb1a20 <hm24csb1a20@student.nitw.ac.in>
2025-10-25 13:46:03 +0530

    added classifcaton models, need to add more data

---
+++ c_file_classifcation.py
@@ -0,0 +1,32 @@
+from cloadfiles import make_the_ml_datasets
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+
+
+def logistic_regressoin_classificaton():
+    X,y=make_the_ml_datasets()
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size= 0.2, stratify=y)
+
+    model = LogisticRegression()
+    model.fit(X=X_train,y=y_train)
+    y_pred = model.predict(X_test)
+    acc= accuracy_score(y_pred=y_pred,y_true=y_test)
+    print(acc)
+
+def random_tree_forest():
+    X,y=make_the_ml_datasets()
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size= 0.2, stratify=y)
+
+    model = RandomForestClassifier()
+    model.fit(X=X_train,y=y_train)
+    y_pred = model.predict(X_test)
+    acc= accuracy_score(y_pred=y_pred,y_true=y_test)
+    print(acc)
+random_tree_forest()
+
+
