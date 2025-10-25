89083a8176fae7d7ede70364e0a1697faeb9cbc1
hm24csb1a20 <hm24csb1a20@student.nitw.ac.in>
2025-10-25 13:25:40 +0530

    made the loader and seems like its works fine

--- cloadfiles.py
+++ cloadfiles.py
@@ -4,7 +4,7 @@ import numpy as np
 from makesyntaxtree import parse_c_file
 from collections import Counter
 from itertools import chain
-
+# predined to save time 
 FEATURE_LIST = ['ARRAY_SUBSCRIPT_EXPR', 'BINARY_OPERATOR', 'BREAK_STMT', 'CALL_EXPR', 'CASE_STMT', 'CHARACTER_LITERAL',
                  'COMPOUND_ASSIGNMENT_OPERATOR', 'COMPOUND_LITERAL_EXPR', 'COMPOUND_STMT', 'CONDITIONAL_OPERATOR', 'CONTINUE_STMT',
                    'CSTYLE_CAST_EXPR', 'CXX_UNARY_EXPR', 'DECL_REF_EXPR', 'DECL_STMT', 'DEFAULT_STMT', 'DO_STMT', 'ENUM_CONSTANT_DECL', 'ENUM_DECL', 
@@ -12,6 +12,18 @@ FEATURE_LIST = ['ARRAY_SUBSCRIPT_EXPR', 'BINARY_OPERATOR', 'BREAK_STMT', 'CALL_E
                      'LABEL_STMT', 'MEMBER_REF_EXPR', 'NULL_STMT', 'PAREN_EXPR', 'PARM_DECL', 'RETURN_STMT', 'STATIC_ASSERT', 'STRING_LITERAL', 'STRUCT_DECL',
                        'SWITCH_STMT', 'TRANSLATION_UNIT', 'TYPEDEF_DECL', 'TYPE_REF', 'UNARY_OPERATOR', 'UNEXPOSED_ATTR', 'UNEXPOSED_DECL', 'UNEXPOSED_EXPR',
                          'UNION_DECL', 'VAR_DECL', 'WHILE_STMT']
+def makeFEATURELIST(filepaht1,filepaht2):
+    """
+    first time running program made this to get the hyperparameter 
+    FEATURE_LIST"""
+    benign_featrues = parse_all_files(filepaht1)  
+    malicious_features =parse_all_files(filepaht2)
+    all_features = benign_featrues+malicious_features  
+
+    temp = set(chain.from_iterable(all_features))
+    FEATURE_LIST = sorted(temp)
+    return(FEATURE_LIST)
+
 
 def getallfiels(file_path):
     """
@@ -19,7 +31,6 @@ def getallfiels(file_path):
     current = os.getcwd()
     root = os.path.join(current,"cpp_tests",file_path)
     return os.listdir(root)
-
 def parse_all_files(file_path):
     """"
     if given {"benign"} or {"malicious"} gets the parses of all those"""
@@ -29,20 +40,40 @@ def parse_all_files(file_path):
     for i in suffixes:
         results.append(parse_c_file(os.path.join(root,i)))
     return (results)
+def vectorize_parse_all_files(file_path):
+    """"
+    if given {"benign"} or {"malicious"} gets the parses of all those"""
+    suffixes = getallfiels(file_path)
+    root = os.path.join(os.getcwd(),"cpp_tests",file_path)
+    results =[]
+    for i in suffixes:
+        results.append(pareser_to_vector(parse_c_file(os.path.join(root,i))))
+    return np.array(results)
 
-def pareser_to_vector(node_kinds):
+def pareser_to_vector(node_kinds,FEATURE_LIST = FEATURE_LIST):
     # gives dictionary of all the words and their freq
     features = Counter(node_kinds)
-    return np.array([features.get(k, 0) for k in features])
+    return np.array([features.get(k, 0) for k in FEATURE_LIST])
 
-def makeFEATURELIST(filepaht1,filepaht2):
-    benign_featrues = parse_all_files(filepaht1)  
-    malicious_features =parse_all_files(filepaht2)
-    all_features = benign_featrues+malicious_features  
+def make_the_ml_datasets(file_path1="benign",file_path2="malicious"):
+    benign_vectors =vectorize_parse_all_files(file_path1)
+    malicious_vectors = vectorize_parse_all_files(file_path2)
+    # labelling for making y 
+    benign_labels = np.zeros(len(benign_vectors), dtype=int)       # 0 = benign
+    malicious_labels = np.ones(len(malicious_vectors), dtype=int)  # 1 = malicious
+    X = np.vstack([benign_vectors, malicious_vectors])
+    y = np.concatenate([benign_labels, malicious_labels])
 
-    temp = set(chain.from_iterable(all_features))
-    FEATURE_LIST = sorted(temp)
-    return(FEATURE_LIST)
+    n = np.arange(X.shape[0])
+    np.random.shuffle(n)
+    # now not just all benign then all malicious 
+    # now random
+    X = X[n]
+    y = y[n]
+    return X,y
+
+
+
 
 
 if __name__ =="__main__":
@@ -54,4 +85,5 @@ if __name__ =="__main__":
 
     # the first time u run the program make the feture list 
     # computationally expnesive so predefining in my case
-    FEATURE_LIST = makeFEATURELIST("benign","malicious")
+    # FEATURE_LIST = makeFEATURELIST("benign","malicious")
+    print(make_the_ml_datasets())
