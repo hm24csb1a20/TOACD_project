fc4e3d5895be4797ecc92c211de14175d415104a
hm24csb1a20 <hm24csb1a20@student.nitw.ac.in>
2025-10-25 12:13:54 +0530

    added c database

--- cpp_tests/benign/21 stl.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include <vector>
-using namespace std;
-int main(){
-    vector<int>v = {1,2,4};
-    cout<<v.size();
-    cout<<v.capacity();
-}
--- cpp_tests/benign/33 repeated string match.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-int main(){
-    string s1= "abcd";
-    string s2="cdabcdab";
-    s1=s1*(s2.size()/s1.size());
-}
